7086338d47f4182fa0d20ec8940aa70241f430f3
Yong-Zhuang <jazz14jazz@gmail.com>
2016-02-17 20:11:53 -0500

    cs681 notes

--- HomePage/helloworld.java
+++
@@ -1,7 +0,0 @@
-public class helloworld {
-	public static void main(String[] args) {
-		System.out.println("test input main arguments");
-		System.out.println(args[0] + "        " + args[1]);
-		System.out.println("end of main");
-	}
-}
